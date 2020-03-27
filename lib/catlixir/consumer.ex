7e5a0142bd7b5e437692cbfb743abce36a743cb1
zastrixarundell <zastrixarundell@gmail.com>
2020-03-27 19:12:15 +0100

    Added docs

--- lib/catlixir/consumer.ex
+++ lib/catlixir/consumer.ex
@@ -1,15 +1,24 @@
 defmodule Catlixir.Consumer do
   @moduledoc """
-  Module which is the Discord consumer. Can be said this is the
-  main module for the Discord bot.
+  Module which is the Discord consumer. This handles all of the events
+  for the bot and then continues with the rest of the logic depending
+  on the event and result.
+
+  This does not directly send messages, it is only a parser.
   """
 
   use Nostrum.Consumer
 
+  @doc false
   def start_link do
     Consumer.start_link(__MODULE__)
   end
 
+  @doc """
+  Handles the `:MESSAGE_CREATE` event. If it is
+  seen as a command crated from an user, it will
+  parse it to the command module.
+  """
   def handle_event({:MESSAGE_CREATE, message, _ws_state}) do
     if !message.author.bot do
       start =
