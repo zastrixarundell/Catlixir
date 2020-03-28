7b1c815a2a5246f390745d1bbefcf4f633552464
zastrixarundell <zastrixarundell@gmail.com>
2020-03-28 02:01:11 +0100

    Checking the cat api key

--- config/config.exs
+++ config/config.exs
@@ -17,5 +17,16 @@ config :nostrum,
   token: token,
   num_shards: if shards != -1, do: shards, else: :auto
 
+# Configuration for Catlixir
+
+the_cat_api_key =
+  System.get_env("THE_CAT_API_KEY") ||
+    raise """
+    The cat api key not found!
+    Get one for free at: https://thecatapi.com/
+    """
+
+
 config :catlixir,
-  command: System.get_env("DISCORD_BOT_COMMAND") || ".cat"
+  command: System.get_env("DISCORD_BOT_COMMAND") || ".cat",
+  the_cat_api_key: the_cat_api_key
