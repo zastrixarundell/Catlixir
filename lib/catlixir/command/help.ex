3b1cc28218576cce9fa6afe8f28f98278a4fdd34
zastrixarundell <zastrixarundell@gmail.com>
2020-03-29 01:19:08 +0100

    Updated help

--- lib/catlixir/command/help.ex
+++ lib/catlixir/command/help.ex
@@ -36,9 +36,10 @@ defmodule Catlixir.Command.Help do
     |> put_field("#{@command} fact", "Get a random fact about us (cats)!")
     |> put_field("#{@command} breed (name)", "Get info about a breed. If the name is not specified, it will return a random breed.")
     |> put_field("#{@command} random", "Get a random image of a cat!")
-    |> put_field("#{@command} invite", "Invite the bot to your server!")
-    |> put_field("#{@command} support", "Go to the support server!")
-    |> put_field("#{@command} help", "Show this menu!")
+    |> put_field("#{@command} meme", "Get a random meme from the r/Catmeme subreddit!")
+    |> put_field("#{@command} invite", "Invite the bot to your server.")
+    |> put_field("#{@command} support", "Go to the support server.")
+    |> put_field("#{@command} help", "Show this menu.")
     |> put_footer("Current version: #{vsn |> List.to_string()}")
     |> put_color_on_embed(message)
   end
