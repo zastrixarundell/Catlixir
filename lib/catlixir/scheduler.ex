b658fb2ebeae0d698aabeb7e51302ed9bad4e9db
zastrixarundell <zastrixarundell@gmail.com>
2020-04-15 01:00:19 +0200

    Posting data to the DBL API

--- lib/catlixir/scheduler.ex
+++ lib/catlixir/scheduler.ex
@@ -1,8 +1,18 @@
 defmodule Catlixir.Scheduler do
   use Quantum, otp_app: :catlixir
 
+  alias DiscordBotList.Struct.BotStats
+
   def update_dbl_status() do
-    :ok
+    number_of_shards = Application.get_env(:nostrum, :num_shards)
+    server_count = Catlixir.get_server_count!()
+
+    stats = %BotStats{
+      shard_count: number_of_shards,
+      server_count: server_count
+    }
+
+    BotStats.post_updated_data(stats: stats)
   end
 
 end
